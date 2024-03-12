93a527e2aa8933dea5c252fd276cb794970c052e
ShammerY <samuel.ibarra1227@gmail.com>
2024-03-12 11:42:00 -0500

    Added Assistant

--- ExperimentationProject/src/Main.java
+++ ExperimentationProject/src/Main.java
@@ -6,5 +6,38 @@ public class Main {
     }
     public static void main(String[] args) {
         Main main = new Main();
+        main.executeProgram();
     }
+    public void print(Object s){
+        System.out.println(s);
+    }
+    public void executeProgram(){
+        String option = "";
+        do{
+            print(mainMenu());
+            option = reader.next();
+            switch(option) {
+                case "1":
+                    print("selected 1");
+                    break;
+                case "2":
+                    print("selected 2");
+                    break;
+                case "0":
+                    print("See you next time");
+                    break;
+                default:
+                    print("\nInvalid Option");
+            }
+        }while(!option.equals("0"));
+    }
+    public String mainMenu(){
+        return  "\n\n-----------MENU-------------"+
+                "\n(1) REGISTER EVENT"+
+                "\n(2) REGISTER ASSISTANT"+
+                "\n(3) PRINT EVENTS"+
+                "\n(4) PRINT ASSISTANTS"+
+                "\n(5) Register Assistant to Event";
+    }
+
 }
